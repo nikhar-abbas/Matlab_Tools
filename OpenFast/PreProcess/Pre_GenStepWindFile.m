a5e40e91b2c502ffcf0f57d35d2efc83325918b3
Nikhar Abbas <nikhar.abbas@colorado.edu>
2019-10-01 11:48:10 -0600

    Update to be generic

--- OpenFast/PreProcess/Pre_GenStepWindFile.m
+++ OpenFast/PreProcess/Pre_GenStepWindFile.m
@@ -1,12 +1,11 @@
-% GenStepWindFile
-% Function for generating a step wind speed file given:
+ % GenStepWindFile
+% Function for generating a wind speed file given:
 % 1) A base filename
 % 2) An array of windspeeds
-% - The generated file will have a name BaseName_Windspeed_MPS.wnd
 %
-%In:    fileName          -   filename for step file
-%       tstep             -   length of timestep between wind changes
-%       windSpeedArray    -   array of wind speeds in m/s
+%In:    fileName          -   filename for output file
+%       time              -   array of timesteps
+%       windspeed         -   array of wind speeds in m/s
 %
 %
 % Paul Fleming, JUNE 2011
@@ -17,17 +16,17 @@
 % run using Pre_GenStepWind.m for even more ease!
 
 
-function GenStepWindFile(fileName,tstep, windSpeed)
+function Pre_GenWindFile(fileName,time, windspeed)
 
 
 fid= fopen(fileName,'w');
-fprintf(fid,'!Wind file with step changes in wind speed. \n');
+fprintf(fid,'!Wind file for input to OpenFAST. \n');
 fprintf(fid,'!Time  Wind     Wind	Vert.       Horiz.      Vert.       LinV        Gust \n');
 fprintf(fid,'!      Speed    Dir    Speed       Shear		Shear       Shear       Speed \n');
-fprintf(fid,'%2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f\n', 0, windSpeed(1), 0, 0,0, 0,0,0);
-for i=1:1:length(windSpeed)
-    fprintf(fid,'%2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f\n', 0.1+tstep*(i-1), windSpeed(i), 0, 0,0, 0,0,0);
-    fprintf(fid,'%2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f\n', tstep*i, windSpeed(i), 0, 0,0, 0,0,0);    
+fprintf(fid,'%2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f\n', 0, windspeed(1), 0, 0,0, 0,0,0);
+for i=1:1:length(windspeed)
+    fprintf(fid,'%2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f\n', time(i), windspeed(i), 0, 0,0, 0,0,0);
+    fprintf(fid,'%2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f\n', time(i), windspeed(i), 0, 0,0, 0,0,0);    
 end
 
 fclose(fid);
--- OpenFast/PreProcess/Pre_GenStepwind.m
+++ OpenFast/PreProcess/Pre_GenStepwind.m
@@ -1,15 +1,28 @@
 % GenStepwind
 %% Generate wind files with this script. 
-% - This just runs Pre_GenStepWind.m, but makes setup a bit easier
-
+% - This runs Pre_GenWindFile.m to generate a step wind file
 
 %% Define Inputs
 outdir = '/Users/nabbas/Documents/TurbineModels/WindFiles/StepWind';
 filename = 'NoShr_9-14_Inc1_50s.wnd';
 windspeeds = [9:14];
 tstep = 50;
+% tlength = 500;
 
+t = 0;
+j = 1;
+for i = 1:length(windspeeds)
+    time(j) = t;
+    windspeedArray(j) = windspeeds(i);
+    j = j+1;
+    t = t+tstep-1
+    
+    time(j) = t;
+    windspeedArray(j) = windspeeds(i);
+    t = t+1;
+    j = j+1;
+end
 
-% Generate wind
-Pre_GenStepWindFile([outdir filesep filename],tstep,windspeeds)
+% Generate step wind file
+Pre_GenWindFile([outdir filesep filename],time,windspeedArray)
 
